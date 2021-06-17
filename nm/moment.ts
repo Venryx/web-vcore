9b23b3938084227df9fdc41716fe0b0b5fab9fce
Venryx <venryx@gmail.com>
2021-06-17 03:49:27 -0700

    * Fixed that "moment" default export was not callable as a function. (found that you can workaround issue by creating a new "union" type, adding the namespace/named-exports with the function definition)

--- nm/moment.ts
+++ nm/moment.ts
@@ -1,4 +1,11 @@
 //export * from "moment";
-//import uPlot = require("uplot");
+/*import moment = require("moment");
+export default moment;*/
+
 import * as moment from "moment";
-export default moment;
+//export default moment;
+
+type MomentFunc = ((inp?: moment.MomentInput, format?: moment.MomentFormatSpecification, strict?: boolean)=>moment.Moment)
+	| ((inp?: moment.MomentInput, format?: moment.MomentFormatSpecification, language?: string, strict?: boolean)=>moment.Moment);
+const moment_fixed = moment as MomentFunc & typeof moment;
+export default moment_fixed;
