b90669c4330bbdcaf50b096b2e638da6d7cca42b
Venryx <venryx@gmail.com>
2021-06-18 06:37:17 -0700

    * Fixed that source-maps were not enabled for the "nm" folder. (so if an error happened through that path, chrome was unable to identify what file it came from -- it would merely say/show "app.js") * Simplified ModuleExportExposer.ts. (now that webpack-runtime-require does much more of the work)

--- Source/Utils/General/ModuleExportExposer.ts
+++ Source/Utils/General/ModuleExportExposer.ts
@@ -1,51 +1,15 @@
 import {Assert, IsString} from "js-vextensions";
-import {ParseModuleData, Require, GetModuleNameFromPath} from "webpack-runtime-require";
-//import {ParseModuleData, Require, GetModuleNameFromPath} from "../../../nm/webpack-runtime-require";
+import {wrr} from "webpack-runtime-require";
 
-export function ExposeModuleExports(addFromVendorDLL = false) {
-	ParseModuleData(true);
-	G({R: Require});
-	const RR = function() { return RR; };
-	RR.all = {}; // holds same values as on RR, except in an object (so you can log RR.all and easily observe the data)
+export function ExposeModuleExports() {
+	wrr.ParseModuleData(true);
 
-	const moduleEntries = (Require as any).Pairs();
+	// this structure seems more complicated than necessary; will clean up later
+	G({
+		R: wrr.moduleExports,
+		RR: wrr.moduleExports_flat,
+		RE: wrr.Require,
+	});
 
-	// add modules from dll-bundle as well
-	if (addFromVendorDLL) {
-		Assert(Require["dll_reference vendor"] != null, "Could not find webpack-data for vendor chunk.");
-		for (const dllEntry of Require["dll_reference vendor"].c.Pairs()) {
-			const moduleName = GetModuleNameFromPath(dllEntry.key);
-			Require[moduleName] = dllEntry.value.exports;
-			moduleEntries.push({key: moduleName, value: dllEntry.value.exports});
-		}
-	}
-
-	for (const {key: moduleName, value: moduleExports} of moduleEntries) {
-		//if (moduleExports == null) continue;
-		if (moduleExports == null || (IsString(moduleExports) && moduleExports == "[failed to retrieve module exports]")) continue;
-
-		function StoreValue(baseKey: string, value: any) {
-			// check for invalid keys
-			if (baseKey == "length") return; // needed fsr
-			if (baseKey.match(/^[0-9]+$/)) return;
-
-			let finalKey = baseKey;
-			while (finalKey in RR) {
-				if (RR[finalKey] === value) break; // if key already holds this value, break (thus doing a noop/reassignment)
-				finalKey += "_";
-			}
-			RR[finalKey] = value;
-			RR.all[finalKey] = value;
-		}
-
-		for (const {key, value} of moduleExports.Pairs()) {
-			StoreValue(key, value);
-		}
-		//let defaultExport = moduleExports.default || moduleExports;
-		if (moduleExports.default) {
-			StoreValue(moduleName, moduleExports.default);
-		}
-	}
-	G({RR});
-	return RR;
+	return wrr;
 }
