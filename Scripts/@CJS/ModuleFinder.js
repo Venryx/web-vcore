927a41e0e9c79d82881997fba0410de15d14c5a4
Venryx <venryx@gmail.com>
2021-07-05 18:18:01 -0700

    * Made ModuleFinder.js more flexible. (can supply "relative to" arg)

--- Scripts/@CJS/ModuleFinder.js
+++ Scripts/@CJS/ModuleFinder.js
@@ -1,11 +1,15 @@
+const process = require("process");
+const paths = require("path");
+const fs = require("fs");
+
 /** Helpful for finding the path to a subdependency of web-vcore, regardless of whether caller: 1) is in monorepo, 2) has web-vcore symlinked */
-module.exports.FindWVCNodeModule = function(name) {
+module.exports.FindWVCNodeModule = function(name, relativeTo = process.cwd()) {
 	// (the below could maybe be simplified through use of __dirname)
 	const pathsToTry = [
-		`./node_modules/web-vcore/node_modules/${name}`,
-		`./node_modules/${name}`,
-		`../../node_modules/web-vcore/node_modules/${name}`,
-		`../../node_modules/${name}`,
+		paths.join(relativeTo, `./node_modules/web-vcore/node_modules/${name}`),
+		paths.join(relativeTo, `./node_modules/${name}`),
+		paths.join(relativeTo, `../../node_modules/web-vcore/node_modules/${name}`),
+		paths.join(relativeTo, `../../node_modules/${name}`),
 	];
 	const path = pathsToTry.find(a=>fs.existsSync(a));
 	if (path == null) throw new Error(`Failed to find web-vcore node-module "${name}".`);
