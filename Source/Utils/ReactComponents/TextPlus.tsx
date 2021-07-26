c106f102b0c0e6dece101a67c6adb7471fb74c5f
Venryx <venryx@gmail.com>
2021-07-26 15:05:38 -0700

    * Fixed that TextPlus comp was having typing issues in parent project. (apparently don't use BaseComponentPlus in web-vcore)

--- Source/Utils/ReactComponents/TextPlus.tsx
+++ Source/Utils/ReactComponents/TextPlus.tsx
@@ -1,9 +1,9 @@
 import React from "react";
-import {ApplyBasicStyles, BaseComponentPlus, BasicStyles} from "react-vextensions";
+import {ApplyBasicStyles, BaseComponent, BaseComponentPlus, BasicStyles} from "react-vextensions";
 import {TextProps, Row, Text} from "react-vcomponents";
 import {InfoButton, InfoButtonProps} from "./InfoButton.js";
 
-export class TextPlus extends BaseComponentPlus({} as {info?: string, infoProps?: InfoButtonProps, sel?: boolean} & TextProps, {}) {
+export class TextPlus extends BaseComponent<{info?: string, infoProps?: InfoButtonProps, sel?: boolean} & TextProps, {}> {
 	render() {
 		const {info, infoProps, sel, children, ...rest} = this.props;
 
