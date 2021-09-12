ef9e628fd7d4c871faa648cc63833dd5dace6146
Venryx <venryx@gmail.com>
2021-09-11 20:45:04 -0700

    * Fixed patch-applying when run from different cwd. (I think anyway...)

--- Scripts/ApplyPatches.ts
+++ Scripts/ApplyPatches.ts
@@ -1,15 +1,24 @@
 import fs from "fs";
+import paths from "path";
 //import {execSync} from "child_process";
 import {createRequire} from "module";
+
+import {dirname} from "path";
+import {fileURLToPath} from "url";
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+//const PathFromWVC = subPath=>paths.join(__dirname, "..", subPath);
+const PathFromWVC = subPath=>paths.resolve(__dirname, "..", subPath).replace(/\\/g, "/"); // patch-package expects forward-slashes (eg. for finding files in the patches-dir)
+
 const require = createRequire(import.meta.url);
 const patchPackagePath =
-	fs.existsSync("./node_modules/patch-package") ? `${process.cwd()}/node_modules/patch-package` :
-	fs.existsSync("../../node_modules/patch-package") ? `${process.cwd()}/../../node_modules/patch-package` :
-	(()=>{ throw new Error(`Could not find patch-package, relative to:${process.cwd()}`); })();
+	fs.existsSync(PathFromWVC("node_modules/patch-package")) ? PathFromWVC("node_modules/patch-package") :
+	fs.existsSync(PathFromWVC("../../node_modules/patch-package")) ? PathFromWVC("../../node_modules/patch-package") :
+	(()=>{ throw new Error(`Could not find patch-package, relative to:${PathFromWVC(".")}`); })();
 const require_patch = subpath=>require(`${patchPackagePath}/dist/${subpath}`);
 
 //console.log("Test1;", process.cwd());
-for (const patchFile of fs.readdirSync("./patches")) {
+for (const patchFile of fs.readdirSync(PathFromWVC("patches"))) {
 	let orgName, packageName;
 	if (patchFile.startsWith("@")) {
 		[, orgName, packageName] = patchFile.match(/@(.+?)\+(.+?)\+/)!;
@@ -19,16 +28,16 @@ for (const patchFile of fs.readdirSync("./patches")) {
 	//console.log("Path:", patchFile, "@Org:", orgName, "@Pkg:", packageName);
 
 	const orgPlusPackageSubpath = orgName ? `@${orgName}/${packageName}` : packageName;
-	const isSubdepUnderWVC = fs.existsSync(`./node_modules/${orgPlusPackageSubpath}`);
-	const isSubdepAsPeer = fs.existsSync(`../${orgPlusPackageSubpath}`);
+	const isSubdepUnderWVC = fs.existsSync(PathFromWVC(`node_modules/${orgPlusPackageSubpath}`));
+	const isSubdepAsPeer = fs.existsSync(PathFromWVC(`../${orgPlusPackageSubpath}`));
 	
 	//let result;
 	if (isSubdepUnderWVC) {
-		console.log(`Applying patch for ${patchFile}, at subdep path.`);
+		console.log(`Applying patch for ${patchFile}, at subdep path: ${PathFromWVC(`node_modules/${orgPlusPackageSubpath}`)}`);
 		ApplyPatch(patchFile, true);
 		//result = execSync(`git apply --ignore-space-change --ignore-whitespace patches/${patchFile}`);
 	} else if (isSubdepAsPeer) {
-		console.log(`Applying patch for ${patchFile}, at peer path.`);
+		console.log(`Applying patch for ${patchFile}, at peer path: ${PathFromWVC(`../../node_modules/${orgPlusPackageSubpath}`)}`);
 		ApplyPatch(patchFile, false);
 		//result = execSync(`cd ../.. && git apply --ignore-space-change --ignore-whitespace node_modules/web-vcore/patches/${patchFile}`);
 	} else {
@@ -43,10 +52,18 @@ function ApplyPatch(patchFile: string, asSubdep: boolean) {
 
 	//const appPath = require_patch("./getAppRootPath").getAppRootPath();
 	//const appPath = orgPlusPackageSubpath.startsWith("..") ? "../.." : ".";
-	const appPath = asSubdep ? process.cwd() : `${process.cwd()}/../..`;
+	const appPath = asSubdep ? PathFromWVC(".") : PathFromWVC("../..");
 	//const reverse = !!argv["reverse"];
 	const reverse = false;
 	//const shouldExitWithError = !!argv["error-on-fail"] || is_ci_1.default || process_1.default.env.NODE_ENV === "test";
 	const shouldExitWithError = false;
-	require_patch("applyPatches.js").applyPatchesForApp({appPath, reverse, patchDir: `${process.cwd()}/patches`, shouldExitWithError});
+	//console.log("Patch dir:", PathFromWVC("patches"));
+	require_patch("applyPatches.js").applyPatchesForApp({
+		appPath,
+		reverse,
+		//patchDir: "./patches",
+		patchDir: paths.relative(appPath, PathFromWVC("patches")), // patch-package wants this relative to app-path
+		//patchDir: asSubdep ? `./node_modules/web-vcore/patches` : `./patches`,
+		shouldExitWithError,
+	});
 }
