d9ceda96c59be7dd1dc949ada2e70695422221d6
Venryx <venryx@gmail.com>
2021-11-26 17:11:42 -0800

    * Fixed webpack/babel warning about "loose" not being set for private-prop-in-object plugin.

--- Scripts/Build/WebpackConfig.ts
+++ Scripts/Build/WebpackConfig.ts
@@ -383,6 +383,7 @@ export function CreateWebpackConfig(opt: CreateWebpackConfig_Options) {
 					],
 					// needed to match with "{loose: false}" for "@babel/plugin-proposal-class-properties"
 					[SubdepPath("@babel/plugin-proposal-private-methods"), {loose: false}],
+					[SubdepPath("@babel/plugin-proposal-private-property-in-object"), {loose: false}],
 				],
 				// needed for mobx @observable (probably) (requires Babel >= 7.13.0: https://babeljs.io/docs/en/assumptions)
 				assumptions: {
